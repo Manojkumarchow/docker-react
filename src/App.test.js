46288498dd42aa3e6c92bece5bd841788b3cd549
Manoj <manojchow72@gmail.com>
2021-08-01 17:15:01 +0530

    1 test removed

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-test('sample link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
